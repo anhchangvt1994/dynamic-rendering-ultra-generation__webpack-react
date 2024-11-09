133c5259503415eb0e989619a83d78df4cbee1df
anhchangvt1994 <anhchangvt1994@gmail.com>
2024-11-09 16:46:00 +0700

    update(other): :jigsaw: update
    update logic of server and README

--- config/webpack.serve.config.js
+++ config/webpack.serve.config.js
@@ -5,15 +5,11 @@ const { getPort, findFreePort } = require('./utils/PortHandler')
 ;(async () => {
 	const puppeteerSSRPort = getPort('PUPPETEER_SSR_PORT') || 8080
 	const port = await findFreePort(process.env.PORT)
+
 	const serverInitial = new WebpackDevServer(
-		webpack({
-			mode: 'development',
-			entry: {},
-			output: {},
-		}),
 		{
 			compress: true,
-			port: port,
+			port,
 			static: './dist',
 			historyApiFallback: true,
 			devMiddleware: { index: false },
@@ -23,7 +19,12 @@ const { getPort, findFreePort } = require('./utils/PortHandler')
 					target: `http://localhost:${puppeteerSSRPort}`,
 				},
 			],
-		}
+		},
+		webpack({
+			mode: 'production',
+			entry: {},
+			output: {},
+		})
 	)
 
 	serverInitial.start()
